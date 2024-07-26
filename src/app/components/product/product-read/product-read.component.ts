3c603bd757d612df8abaf11fefb351ab666bbd0e
LionesBR144 <isaacleonmarques11@gmail.com>
2024-07-26 17:17:26 -0300

    Modificações efetuadas em confirm-dialog.component.css, confirm-dialog.component.html e 2 outros arquivos...

--- src/app/components/product/product-read/product-read.component.ts
+++ src/app/components/product/product-read/product-read.component.ts
@@ -3,6 +3,7 @@ import { Product } from '../product-create/product.model';
 import { ProductService } from '../product.service';
 import { ConfirmDialogComponent, ConfirmDialogModel } from '../../confirm-dialog/confirm-dialog.component';
 import { MatDialog } from '@angular/material/dialog';
+import { Router } from '@angular/router';
 
 @Component({
   selector: 'app-product-read',
@@ -14,29 +15,48 @@ export class ProductReadComponent implements OnInit {
   products: Product[]
   displayedColumns = ['id','name','price', 'action']
   result: string = '';
+  product: Product;
 
-  constructor(private productService: ProductService,public dialog: MatDialog) { }
+  constructor(private productService: ProductService,
+    public dialog: MatDialog,
+    private router: Router
+  ) { }
 
   ngOnInit(): void {
-    this.productService.read().subscribe(products => {
-      this.products = products
-      console.log(products)
-    })
+    this.getList();
   }
 
-  confirmDialog(): void {
-    const message = `Are you sure you want to do this?`;
+  confirmDialog(id: number): void {
+    const message = `Deseja realmente excluir?`;
 
-    const dialogData = new ConfirmDialogModel("Confirm Action", message);
+    const dialogData = new ConfirmDialogModel("Confirmar Exclusão", message);
 
     const dialogRef = this.dialog.open(ConfirmDialogComponent, {
-      maxWidth: "400px",
+      width: "350px",
+      height:"250px",
       data: dialogData
     });
 
     dialogRef.afterClosed().subscribe(dialogResult => {
-      this.result = dialogResult;
+      console.log("[ConfirmDialog()]", dialogResult)
+      if (dialogResult) {
+        this.deleteProduct(id);
+      }
     });
   }
-         
+
+  deleteProduct(id: number): void {
+    this.productService.delete(id).subscribe(() => {
+    this.productService.showMessage('Produto Excluido');
+    this.router.navigate(['/products']);
+      this.getList()
+    });
+  }     
+
+  getList() {
+    this.productService.read().subscribe(products => {
+      this.products = products
+           console.log(products)
+        })
+  }
 }
